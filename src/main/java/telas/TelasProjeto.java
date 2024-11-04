d8779e9e2b246fa29b0b8fc761f636e0ed36ed3d
A.Farias <121917866+art-hur00@users.noreply.github.com>
2024-11-04 11:31:01 -0300

    Add files via upload

---
+++ src/main/java/telas/TelasProjeto.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.telasprojeto;
+
+/**
+ *
+ * @author arthu
+ */
+public class TelasProjeto {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
